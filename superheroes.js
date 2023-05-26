84b4a1bb4b24b870acebf1e70b7fc392ad6b65d3
Milu Santinoni <121461288+MileSantinoni@users.noreply.github.com>
2023-05-26 05:09:09 -0300

    Add files via upload

---
+++ superheroes.js
@@ -0,0 +1,126 @@
+superheroes = [
+    {
+        "superheroe": "Doctor Strange",
+        "foto": "https://i.pinimg.com/564x/ff/2e/e8/ff2ee859391bf10ffc3bed4cda7de382.jpg",
+        "alias": "Hechicero supremo",
+        "firma": "Marvel Comics",
+        "primera_aparicion": "Doctor Strange (2016)",
+        "ocupación": [
+            "Neurocirujano",
+            "Mago"
+        ],
+        "resumen": "El Doc carece realmente de verdaderos Superpoderes, en su lugar posee vastos poderes místicos (teletransportación, generación de ilusiones, proyección de energía), cuyo nivel puede compararse a las de algunas entidades cósmicas. Sus fuentes principales son la energía mística ambiental y las derivados de la Mente, el Alma y el cuerpo, pero puede obtener poder adicional por entidades místicas con quienes haya hecho tratos, además de poder invocar algunos de sus poderes, como las llamas de los Faltine, las bandas carmesíes de Cyttorak y el escudo de los serafin."
+    },
+    {
+        "superheroe": "Wanda Maximoff",
+        "foto":"https://i.pinimg.com/564x/63/2e/f1/632ef1223fd0da41103571a35d869d77.jpg",
+        "firma": "Marvel Comics",
+        "alias": "Bruja Escarlata",
+        "primera_aparicion": "Captain America: The Winter Soldier (2014)",
+        "ocupación": [
+            "Maga del caos",
+            "Mamá de Billy y Tommy"
+        ],
+        "resumen": "Wanda tiene la capacidad de aprovechar la magia del caos, que normalmente se presenta en telequinesis, telepatía y manipulación / proyección de energía. En una escena posterior a los créditos de Captain America: The Winter Soldier, se la ve experimentando con sus habilidades recién obtenidas en una celda junto a su hermano, levitando bloques sólidos antes de aplastarlos con telequinesis. Para cuando se la presenta en Avengers: Age of Ultron, se revela que los poderes de Wanda son principalmente telequinéticos y semi-telepáticos. Ella no solo puede mover objetos con su mente, sino que también puede participar en la hipnosis, induciendo imágenes en las cabezas de cuatro Vengadores. También muestra momentos de proyección de energía, incluso cuando eliminó un ejército de drones de Ultron con una ola de energía y creó escudos de energía para protegerse a sí misma y a los ciudadanos de Sokovia del fuego cruzado. Posteriormente, en el nuevo complejo de los Vengadores, se revela que aprendió a levitar. "
+    },
+    {
+        "superheroe": "Black Widow",
+        "foto":"https://i.pinimg.com/564x/52/99/c1/5299c14cc7816606943a66e143a70f4d.jpg",
+        "firma": "Marvel Comics",
+        "alias": "Natasha Romanoff",
+        "primera_aparicion": "Iron Man 2 (2010)",
+        "ocupación": [
+            "Espía",
+            "Bailarina",
+            "Superheroína"
+        ],
+        "resumen": "Natasha Romanoff alias La Viuda Negra fue mejorada Biotecnología lo que hace que su cuerpo sea resistente al envejecimiento y las enfermedades y se recupera por encima de la tasa humana; así como el condicionamiento psicológico que suprime su memoria de los eventos verdaderos en lugar de los implantados del pasado sin la ayuda de medicamentos supresores del sistema especialmente diseñados. Los glóbulos blancos en su cuerpo son lo suficientemente eficientes para combatir cualquier microbio, cuerpo extraño y otros de su cuerpo, manteniéndola sana e inmune a la mayoría, si no a todas las infecciones, enfermedades y trastornos"
+    },
+    {
+        "superheroe": "Iron man",
+        "foto":"https://i.pinimg.com/564x/99/5c/bb/995cbb52e426f7096716a26095fa6156.jpg",
+        "firma": "Marvel Comics",
+        "alias": "Tony Stark",
+        "primera_aparicion": "Iron Man (2008)",
+        "ocupación": [
+            "Magnate",
+            "Científico",
+            "Ingeniero"
+        ],
+        "resumen":"ron Man posee una armadura motorizada que le brinda fuerza y resistencia sobrehumanas, le permite volar y le concede acceso a una gran variedad de armas. La armadura es inventada y usada por Stark (con excepciones breves de usuarios ocasionales). Los sistemas de armas del traje han cambiado con los años, pero conservando las armas estándar de Iron Man: los rayos repulsores que se disparan desde las palmas de sus guanteletes. Otras armas incorporadas en varias encarnaciones de la armadura incluyen: el proyector de haz único en su pecho; pernos de pulso (que acumulan energía cinética en el camino; por lo tanto, mientras más lejos viajan, más fuerte golpean); un generador de impulsos electromagnéticos; y un escudo de energía defensivo que puede extenderse hasta 360 grados. Otras capacidades incluyen: generar ultra-freón (es decir, un haz de congelación); creando y manipulando campos magnéticos; emisión de explosiones sonoras; y proyectando hologramas tridimensionales (para crear señuelos)."
+    },
+    {
+        "superheroe": "Ant-Man",
+        "foto":"https://i.pinimg.com/564x/db/ae/a4/dbaea4dda2e8678757e8ba800a14c93c.jpg",
+        "firma": "Marvel Comics",
+        "alias": "Scott Lang",
+        "primera_aparicion": "Ant-Man (2015)",
+        "ocupación": [
+            "Ladrón reformado",
+            "Experto en electrónica",
+            "Miembro de los vengadores"
+        ]
+    },
+    {
+        "superheroe": "Rocket Raccoon",
+        "foto":"https://i.pinimg.com/564x/bf/c6/75/bfc6756ef7ac38ff8177a29fa012cdb6.jpg",
+        "firma": "Marvel Comics",
+        "alias": "Rocky Raccoon",
+        "primera_aparicion": "Guardianes de la Galaxia (2014)",
+        "ocupación": [
+            "Caza recompenzas",
+            "Guardián de la Galaxia"
+        ],
+        "resumen": "Rocket Raccoon posee los atributos normales de un mapache de la Tierra, incluido la velocidad (que además ha sido amplificada por su entrenamiento), y un agudo sentido del olfato, vista, oído y tacto. Las garras afiladas le permiten escalar paredes, edificios y árboles con facilidad. Es un piloto consumado de astronaves, un brillante ingeniero y técnico, y un tirador experto con las dos pistolas láser que lleva, además de tener afinidad por las armas pesadas. Es un maestro táctico y líder militar, atributos que lo ayudan a hacerse cargo de los Guardianes de la Galaxia cuando Star-Lord no está disponible."
+
+    },
+    {
+        "superheroe": "Capitán América",
+        "foto":"https://i.pinimg.com/564x/f1/85/e5/f185e550b2250c4227e2b14b9e9f9112.jpg" ,
+        "firma": "Marvel Comics",
+        "alias": "Steve Rogers",
+        "primera_aparicion": "Capitán América: el primer vengador (2011)",
+        "ocupación": [
+            "Súper soldado",
+            "Advenger"
+        ],
+        "resumen": "La experiencia de batalla y el entrenamiento de Rogers lo convierten en un experto táctico y un excelente comandante de campo, ya que sus compañeros de equipo con frecuencia respetan sus órdenes en la batalla. Thor ha declarado que Rogers es uno de los pocos humanos de los que recibirá órdenes y seguirá a través de las puertas de Hades. Aunque el suero del supersoldado es una parte importante de su fuerza, Rogers se ha mostrado todavía suficientemente capaz contra oponentes más fuertes, incluso cuando el suero ha sido desactivado volviéndolo a su físico anterior al Capitán América."
+    },
+    {
+        "superheroe": "Black Panter",
+        "foto":"https://i.pinimg.com/564x/4d/59/8d/4d598db4a68974c6e5d8f6749c23df4c.jpg",
+        "firma": "Marvel Comics",
+        "alias": "T'Challa",
+        "primera_aparicion": "Capitán América: Civil War (2016)",
+        "ocupación": [
+            "Rey de Wakanda",
+            "Superhéroe"
+        ],
+        "resumen":"El título de Pantera Negra es un rango de cargo, jefe del Clan de la Pantera de Wakanda. Como jefe, la Pantera tiene derecho a comer una hierba especial en forma de corazón que le otorga sentidos sobrehumanos agudos, fuerza mejorada, velocidad, agilidad, resistencia, durabilidad, curación y reflejos.Desde entonces, ha perdido esta conexión y ha forjado una nueva con otra deidad de la Pantera desconocida, que le otorga atributos físicos aumentados, así como una resistencia a la magia."
+    },
+    {
+        "superheroe": "Shuri",
+        "foto":"https://i.pinimg.com/564x/7b/fb/9d/7bfb9d6abe39a45c4ca0e492931582eb.jpg",
+        "firma": "Marvel Comics",
+        "alias": "Princesa Shuri",
+        "primera_aparicion": "Black Panther (2018)",
+        "ocupación": [
+            "Princesa de Wakanda",
+            "New Black Panter",
+            
+        ],
+        "resumen":"Antes de someterse a las pruebas para convertirse en la Pantera Negra, Shuri era una artista marcial entrenada extensivamente. Después de las pruebas, como las Panteras Negras antes que ella, Shuri consume hierba en forma de corazón; esto le concedió mayor velocidad, agilidad, fuerza, resistencia y sentidos. Su uniforme está compuesto de vibranio."
+    },
+    {
+        "superheroe": "Okoye",
+        "foto":"https://i.pinimg.com/564x/64/f9/87/64f987a8d9517fdd48ac3bf3282588ff.jpg",
+        "firma": "Marvel Comics",
+        "alias": "Okoye fundadora de las Ángeles de Medianoche",
+        "primera_aparicion": "Black Panther (2018)",
+        "ocupación": [
+            "Guerrera de Wakanda",
+            "Angel de Medianoche"
+        ],
+        "resumen":"Okoye es altamente hábil en múltiples formas de combate, siendo uno de los miembros clave de Dora Milaje. Ella es excepcional en el uso de varias armas y herramientas wakandianas."
+    }
+]
